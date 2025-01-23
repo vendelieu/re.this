1db600cb5ca5bed2ba1cb0eb30bf5f773609d098
vendelieu <vendelieu@gmail.com>
2025-01-23 07:24:25 +0300

    little improvements

--- benchmarks/src/main/kotlin/eu/vendeli/rethis/benchmarks/JedisBenchmark.kt
+++ benchmarks/src/main/kotlin/eu/vendeli/rethis/benchmarks/JedisBenchmark.kt
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit
 @Warmup(iterations = 2, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @Timeout(time = 10, timeUnit = TimeUnit.SECONDS)
-@Fork(1, jvmArgsAppend = ["-Xms500m", "-Xmx12g", "-Xss2m", "-XX:MaxMetaspaceSize=1g"])
+@Fork(1, jvmArgsAppend = ["-Xms12g", "-Xmx12g", "-Xss2m", "-XX:MaxMetaspaceSize=1g"])
 class JedisBenchmark {
     private lateinit var jedis: JedisPooled
 
