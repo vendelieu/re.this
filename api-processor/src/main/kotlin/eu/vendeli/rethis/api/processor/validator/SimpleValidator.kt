e096e10c0f003042e7e263fc1d9ea8ccfcad79ae
vendelieu <vendelieu@gmail.com>
2025-05-16 01:35:55 +0300

    improve spec tree builder

--- api-processor/src/main/kotlin/eu/vendeli/rethis/api/processor/validator/SimpleValidator.kt
+++ api-processor/src/main/kotlin/eu/vendeli/rethis/api/processor/validator/SimpleValidator.kt
@@ -1,7 +1,7 @@
 package eu.vendeli.rethis.api.processor.validator
 
 import com.squareup.kotlinpoet.ksp.toClassName
-import eu.vendeli.rethis.api.processor.type.LibSpecNode
+import eu.vendeli.rethis.api.processor.type.LibSpecTree
 import eu.vendeli.rethis.api.processor.type.SpecNode
 import eu.vendeli.rethis.api.processor.type.SpecNodeVisitor
 import eu.vendeli.rethis.api.processor.type.ValidationContext
@@ -10,29 +10,34 @@ import eu.vendeli.rethis.api.spec.common.annotations.RedisOptional
 
 internal object SimpleValidator : SpecNodeVisitor {
     override fun visitSimple(node: SpecNode.Simple, ctx: ValidationContext) {
-        ctx.markProcessed(node.specName)
         val param = ctx.findParam(node.specName) ?: return
         val pType = param.symbol.type.resolve()
         val contextualOptional = checkContextualOptionality(param)
 
-        if (node.optional && !contextualOptional)
+        if (node.optional && !contextualOptional) {
             ctx.reportError("${node.specName}: should be optional (marked with @RedisOptional and nullable or vararg)")
+        }
 
-        if (node.multiple && !param.symbol.isVararg && !pType.isCollection())
+        if (node.multiple && !param.symbol.isVararg && !pType.isCollection()) {
             ctx.reportError("${node.specName}: should be vararg or Collection")
-
-        if (node.token != null && !ctx.isTokenPresent(node.token)) {
-            ctx.reportError("Token ${node.token} is not present")
         }
 
         val expected = node.type.specTypeNormalization()
         val actual = pType.toClassName().simpleName.lowercase().libTypeNormalization()
-        if (expected != actual)
+        if (expected != actual) {
             ctx.reportError("${node.specName}: expected type '$expected', found '$actual'")
+        }
+
+        if (node.token != null && !ctx.isTokenPresent(node.token)) {
+            ctx.reportError("Token ${node.token} is not present")
+            return
+        }
+
+        ctx.markProcessed(node.specName)
     }
 
-    private tailrec fun checkContextualOptionality(node: LibSpecNode?): Boolean = when {
-        node is LibSpecNode.ParameterNode &&
+    private tailrec fun checkContextualOptionality(node: LibSpecTree?): Boolean = when {
+        node is LibSpecTree.ParameterNode &&
             node.symbol.hasAnnotation<RedisOptional>() && (node.symbol.isVararg || node.symbol.type.resolve().isMarkedNullable)
             -> true
 
