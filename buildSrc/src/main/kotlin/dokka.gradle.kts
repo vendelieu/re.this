93a30dfc273e7dc92f35ca14562775a89ea316f2
Vendelieu <j@vendeli.eu>
2025-09-02 06:55:31 +0400

    change dokka

--- buildSrc/src/main/kotlin/dokka.gradle.kts
+++ buildSrc/src/main/kotlin/dokka.gradle.kts
@@ -4,16 +4,19 @@ import java.time.LocalDate
 plugins {
     id("org.jetbrains.dokka")
 }
+
 dokka {
-    moduleName = "re.this"
+    // for correct displaying of dokka project name
+    moduleName = if (project.name != "docs") project.name else "re.this"
     dokkaSourceSets.configureEach {
         sourceRoots.setFrom(project.projectDir.resolve("src/$name/kotlin"))
         sourceLink {
             localDirectory.set(file("src/$name/kotlin"))
-            remoteUrl("$REPO_URL/blob/master/src/$name/kotlin")
+            remoteUrl("$REPO_URL/blob/master/${project.name}/src/$name/kotlin")
             remoteLineSuffix.set("#L")
         }
     }
+
     pluginsConfiguration.html {
         customAssets.from(rootDir.resolve("assets/logo-icon.svg"))
         footerMessage.set("© ${LocalDate.now().year} Vendelieu")
