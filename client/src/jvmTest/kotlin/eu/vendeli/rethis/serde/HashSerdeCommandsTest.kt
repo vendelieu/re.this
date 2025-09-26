554c11c134edefa2c8b6ebf9451a7fc01eac87e6
Vendelieu <j@vendeli.eu>
2025-09-27 01:35:00 +0400

    add hmget test

--- client/src/jvmTest/kotlin/eu/vendeli/rethis/serde/HashSerdeCommandsTest.kt
+++ client/src/jvmTest/kotlin/eu/vendeli/rethis/serde/HashSerdeCommandsTest.kt
@@ -3,6 +3,7 @@ package eu.vendeli.rethis.serde
 import eu.vendeli.rethis.ReThisTestCtx
 import eu.vendeli.rethis.command.hash.hDel
 import eu.vendeli.rethis.command.serde.hGet
+import eu.vendeli.rethis.command.serde.hMGet
 import eu.vendeli.rethis.command.serde.hSet
 import eu.vendeli.rethis.command.serde.hVals
 import io.kotest.matchers.shouldBe
@@ -29,6 +30,8 @@ class HashSerdeCommandsTest : ReThisTestCtx() {
         val serializedResult = client.hGet(key, field, serializer)
         serializedResult shouldBe entity
     }
+    
+    
 
     @Test
     suspend fun `test hVals`() {
@@ -48,5 +51,24 @@ class HashSerdeCommandsTest : ReThisTestCtx() {
         client.hDel(key, field1)
         val serializedResult = client.hVals(key, TestData.serializer())
         serializedResult.single() shouldBe TestData(entity.first, entity.second)
+    }
+
+    @Test
+    suspend fun `test hMGet`() {
+        val key = "testKey6"
+        val field1 = "testField1"
+        val value1 = entity
+
+        val field2 = "testField2"
+        val value2 = entity
+
+        client.hSet(key, field1 to value1, field2 to value2)
+
+        val result = client.hMGet<String>(key, field1, field2)
+        result shouldBe listOf(encodedEntity, encodedEntity)
+
+        val serializer = PairSerializer(String.serializer(), Int.serializer())
+        val serializedResult = client.hMGet(key, field = listOf(field1, field2).toTypedArray(), serializer)
+        serializedResult shouldBe listOf(entity, entity)
     }
 }
