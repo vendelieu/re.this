9e29b0e04efdc8759778d1ce4f8a8230acf1f917
Vendelieu <j@vendeli.eu>
2026-02-24 04:42:43 +0400

    return serde tests

--- client/src/jvmTest/kotlin/eu/vendeli/rethis/serde/StringSerdeCommandsTest.kt
+++ client/src/jvmTest/kotlin/eu/vendeli/rethis/serde/StringSerdeCommandsTest.kt
@@ -1,49 +1,49 @@
-// package eu.vendeli.rethis.serde
-//
-// import eu.vendeli.rethis.ReThisTestCtx
-// import eu.vendeli.rethis.command.serde.get
-// import eu.vendeli.rethis.command.serde.mGet
-// import eu.vendeli.rethis.command.serde.mSet
-// import eu.vendeli.rethis.command.serde.set
-// import eu.vendeli.rethis.utils.serdeModule
-// import io.kotest.matchers.shouldBe
-//
-// class StringSerdeCommandsTest : ReThisTestCtx() {
-//    private val encodedEntity = """{"first":"testValue","second":2}"""
-//    private val entity = TestData("testValue", 2)
-//
-//    @Test
-//    suspend fun `test set and get`() {
-//        val key = "testKeySet"
-//
-//        client.set(key, entity)
-//
-//        // Test retrieving as JSON string
-//        val jsonResult = client.get<String>(key)
-//        jsonResult shouldBe encodedEntity
-//
-//        // Test retrieving as deserialized object
-//        val objResult = client.get<TestData>(key)
-//        objResult shouldBe entity
-//    }
-//
-//    @Test
-//    suspend fun `test mSet and mGet`() {
-//        val key1 = "testKeyM1"
-//        val key2 = "testKeyM2"
-//        val value2 = TestData(key2, 8)
-//
-//        client.mSet(key1 to entity, key2 to value2)
-//
-//        // Test multi-get as JSON strings
-//        val jsonResults = client.mGet<String>(key1, key2)
-//        jsonResults shouldBe listOf(
-//            encodedEntity,
-//            client.serdeModule().serialize(TestData.serializer(), value2),
-//        )
-//
-//        // Test multi-get as objects
-//        val objResults = client.mGet<TestData>(key1, key2)
-//        objResults shouldBe listOf(entity, value2)
-//    }
-// }
+ package eu.vendeli.rethis.serde
+
+ import eu.vendeli.rethis.ReThisTestCtx
+ import eu.vendeli.rethis.command.serde.get
+ import eu.vendeli.rethis.command.serde.mGet
+ import eu.vendeli.rethis.command.serde.mSet
+ import eu.vendeli.rethis.command.serde.set
+ import eu.vendeli.rethis.utils.serdeModule
+ import io.kotest.matchers.shouldBe
+
+ class StringSerdeCommandsTest : ReThisTestCtx() {
+    private val encodedEntity = """{"first":"testValue","second":2}"""
+    private val entity = TestData("testValue", 2)
+
+    @Test
+    suspend fun `test set and get`() {
+        val key = "testKeySet"
+
+        client.set(key, entity)
+
+        // Test retrieving as JSON string
+        val jsonResult = client.get<String>(key)
+        jsonResult shouldBe encodedEntity
+
+        // Test retrieving as deserialized object
+        val objResult = client.get<TestData>(key)
+        objResult shouldBe entity
+    }
+
+    @Test
+    suspend fun `test mSet and mGet`() {
+        val key1 = "testKeyM1"
+        val key2 = "testKeyM2"
+        val value2 = TestData(key2, 8)
+
+        client.mSet(key1 to entity, key2 to value2)
+
+        // Test multi-get as JSON strings
+        val jsonResults = client.mGet<String>(key1, key2)
+        jsonResults shouldBe listOf(
+            encodedEntity,
+            client.serdeModule().serialize(TestData.serializer(), value2),
+        )
+
+        // Test multi-get as objects
+        val objResults = client.mGet<TestData>(key1, key2)
+        objResults shouldBe listOf(entity, value2)
+    }
+ }
--- shared/src/commonMain/kotlin/eu/vendeli/rethis/shared/utils/RTypeUtils.kt
+++ shared/src/commonMain/kotlin/eu/vendeli/rethis/shared/utils/RTypeUtils.kt
@@ -168,8 +168,8 @@ inline fun <reified T> RType.unwrap(): T? {
         this is F64 -> if (T::class == Double::class) value as T else null
         this is BigNumber -> if (T::class == BigInteger::class) value as T else null
         this is VerbatimString -> if (T::class == String::class) value as T else null
-        this is BulkString -> if (T::class == ByteArray::class) value as T else null
-        this is BulkString -> if (T::class == String::class) value.readString() as T else null
+        this is BulkString -> if (T::class == Buffer::class) value as T else null
+        this is BulkString -> if (T::class == String::class) value.readLineStrict() as T else null
         else -> {
             __ParserLogger.warn("Wrong unwrapping [common] method used for $this")
             null
