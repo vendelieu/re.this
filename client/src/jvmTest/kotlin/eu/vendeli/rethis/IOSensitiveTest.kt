54f9ee219e58e11e89518e1be12bbaa4c9224db4
Vendelieu <j@vendeli.eu>
2026-02-27 06:02:52 +0400

    improve test flow

--- client/src/jvmTest/kotlin/eu/vendeli/rethis/IOSensitiveTest.kt
+++ client/src/jvmTest/kotlin/eu/vendeli/rethis/IOSensitiveTest.kt
@@ -5,5 +5,5 @@ import io.kotest.core.spec.Spec
 import kotlin.reflect.KClass
 
 class IOSensitiveTest : Condition {
-    override fun evaluate(kclass: KClass<out Spec>): Boolean = TestEnv.has("GITHUB_ACTOR")
+    override fun evaluate(kclass: KClass<out Spec>): Boolean = !TestEnv.has("GITHUB_ACTOR")
 }
