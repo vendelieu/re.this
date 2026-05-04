c71df2d909bc33764123a6cbd0250291eefa2a99
Vendelieu <j@vendeli.eu>
2026-05-04 08:48:57 +0400

    refactor: adjust IO dispatcher thread limit logic for native and update CI lint/detekt workflows

--- client/src/nativeMain/kotlin/eu/vendeli/rethis/utils/CommonUtils.native.kt
+++ client/src/nativeMain/kotlin/eu/vendeli/rethis/utils/CommonUtils.native.kt
@@ -6,9 +6,17 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.runBlocking
+import kotlin.experimental.ExperimentalNativeApi
+import kotlin.native.Platform.getAvailableProcessors
+
+/**
+ * As in [Dispatchers.IO], it defaults to the limit of 64 threads or the number of cores (whichever is larger).
+ */
+@OptIn(ExperimentalNativeApi::class)
+private val parallelismLimit = maxOf(getAvailableProcessors(), 64)
 
 @OptIn(DelicateCoroutinesApi::class)
 actual val Dispatchers.IO_OR_UNCONFINED: CoroutineDispatcher
-    get() = newFixedThreadPoolContext(nThreads = 200, name = "IO")
+    get() = newFixedThreadPoolContext(nThreads = parallelismLimit, name = "IO")
 
 actual fun <T> coRunBlocking(block: suspend CoroutineScope.() -> T) = runBlocking(block = block)
